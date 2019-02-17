744caf7c26a72c73424f35c0fe6dcec069c42670
Anton Briganti <anton.briganti@c02ql524g8wp.myob.myobcorp.net>
2019-02-18 10:51:33 +1100

    added methods to Paysli generator

--- src/com/company/PayslipGenerator.java
+++ src/com/company/PayslipGenerator.java
@@ -10,20 +10,39 @@ public class PayslipGenerator {
     }
 
     public Payslip createPayslip(Employee employee){
-        long grossIncome = Math.round(employee.getAnnualSalary()/12);
+        long grossIncome = calculateGrossIncome(employee.getAnnualSalary());
 
-        long incomeTax = 0;
-        for(TaxBracket bracket : taxBrackets){
-            if(employee.getAnnualSalary() < bracket.getMaxIncome()){
-                incomeTax = Math.round((bracket.getBaseTax() + (employee.getAnnualSalary() - bracket.getMinIncome()) * bracket.getTaxRate())/12);
-                break;
-            }
-        }
+        long incomeTax = calculateIncomeTax(employee.getAnnualSalary());
 
-        long netIncome = grossIncome - incomeTax;
+        long netIncome = calculateNetIncome(grossIncome, incomeTax);
 
-        long superAmount = Math.round(grossIncome * employee.getSuperRate());
+        long superAmount = calculateSuperAmount(grossIncome, employee.getSuperRate());
 
         return new Payslip(employee, grossIncome, incomeTax, netIncome, superAmount);
     }
+
+    private long calculateGrossIncome(double annualSalary) {
+        return Math.round(annualSalary/12);
+    }
+
+    private long calculateIncomeTax(double annualSalary) {
+        long incomeTax = 0;
+        for(TaxBracket bracket : taxBrackets){
+            if(annualSalary < bracket.getMaxIncome()){
+                incomeTax = Math.round((bracket.getBaseTax() + (annualSalary - bracket.getMinIncome()) * bracket.getTaxRate())/12);
+                break;
+            }
+        }
+        return incomeTax;
+    }
+
+    private long calculateNetIncome(long grossIncome, long incomeTax) {
+        return grossIncome - incomeTax;
+    }
+
+    private long calculateSuperAmount(long grossIncome, double superRate) {
+        return Math.round(grossIncome * superRate);
+    }
+
+
 }
